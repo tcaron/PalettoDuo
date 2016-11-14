cd7007f03b855d9146b9fa528bdd56f161edb1da
tcaron <thomas.caron70@gmail.com>
2016-11-14 20:01:46 +0100

    modif du code + test 4

--- test/test.js
+++ test/test.js
@@ -5,11 +5,34 @@ var PalettoTestCase = TestCase("PalettoTestCase");
 var EngineTest = new Engine();
 PalettoTestCase.prototype.testStory1 = function () {
 
-    assertTrue(EngineTest.juxtapositionInit());
+    assertTrue(EngineTest.check_juxtaposition());
 };
 
 
 PalettoTestCase.prototype.testStory2 = function () {
 
-    assertTrue(EngineTest.check_corner('jaune'));
+    assertTrue(EngineTest.is_in_corner("jaune"));
+
 };
+
+
+
+PalettoTestCase.prototype.testStory3 = function () {
+
+    EngineTest.play("jaune");
+    assertTrue(EngineTest.get_pieces() == 35);
+
+
+
+}
+
+
+PalettoTestCase.prototype.testStory4 = function(){
+
+    EngineTest.play("noir");
+    assertTrue(EngineTest.get_player_pieces(2,"noir") ==2);
+
+    assertTrue(EngineTest.get_pieces() ==  33);
+
+
+}
